65f38f5a622756ea529539ee73a28d9b93e147bc
ziadwagdy <zwagdy@gmail.com>
2023-01-03 19:43:12 +0300

    updated validate to make functions reviece values

--- add-blog/js/validateForm.js
+++ add-blog/js/validateForm.js
@@ -1,64 +1,78 @@
-const formData = {}
+const formData = {};
 
-export default function validateForm(titleInput, contentInput, authorNameInput, id) {
-    const validTitle = isTitleValid(titleInput);
-    const validAuthor = isAuthorValid(authorNameInput);
-    const validContent = isContentValid(contentInput);
-    if (validTitle && validContent && validAuthor) {
-        formData["id"] = id.value;
-        return formData;
-    } else {
-        return null;
-    }
+export default function validateForm(
+  titleInput,
+  contentInput,
+  authorNameInput,
+  id
+) {
+  const title = titleInput.value;
+  const validTitle = isTitleValid(title);
+
+  if (validTitle) {
+    showValid(titleInput);
+  } else {
+    showInvalid(titleInput);
+  }
+
+  const authorName = authorNameInput.value;
+  const validAuthor = isAuthorValid(authorName);
+
+  if (validAuthor) {
+    showValid(authorNameInput);
+  } else {
+    showInvalid(authorNameInput);
+  }
+
+  const content = contentInput.value;
+  const validContent = isContentValid(content);
+  if (validContent) {
+    showValid(contentInput);
+  } else {
+    showInvalid(contentInput);
+  }
+
+  if (validTitle && validContent && validAuthor) {
+    formData["id"] = id.value;
+    return formData;
+  } else {
+    return null;
+  }
 }
-// todo change the  @param to values 
-function isTitleValid(titleInput) {
-    const title = titleInput.value;
-    if (title !== "" && title.trim().length >= 3) {
-        formData["title"] = title;
-        showValid(titleInput);
-        return true;
-    } else {
-        showInvalid(titleInput);
-        return false;
-    }
+// todo change the  @param to values
+function isTitleValid(title) {
+  if (title !== "" && title.trim().length >= 3) {
+    formData["title"] = title;
+    return true;
+  } else {
+    return false;
+  }
 }
 
-
-function isContentValid(contentInput) {
-    const content = contentInput.value;
-    if (content !== "" && content.trim().length >= 15) {
-        formData["content"] = content;
-        showValid(contentInput);
-        return true;
-    } else {
-        showInvalid(contentInput);
-        return false;
-    }
+function isContentValid(content) {
+  if (content !== "" && content.trim().length >= 15) {
+    formData["content"] = content;
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function isAuthorValid(authorNameInput) {
-    const authorName = authorNameInput.value;
-    if (authorName !== "" && authorName.trim().length >= 5) {
-        formData["authorName"] = authorName;
-        showValid(authorNameInput);
-        return true;
-    } else {
-        showInvalid(authorNameInput);
-        return false;
-    }
+function isAuthorValid(authorName) {
+  if (authorName !== "" && authorName.trim().length >= 5) {
+    formData["authorName"] = authorName;
+    return true;
+  } else {
+    return false;
+  }
 }
-
-
-
 
 function showValid(node) {
-    node.parentElement.querySelector(".invalid-feedback").style.display = "none";
-    node.parentElement.querySelector(".valid-feedback").style.display = "block";
+  node.parentElement.querySelector(".invalid-feedback").style.display = "none";
+  node.parentElement.querySelector(".valid-feedback").style.display = "block";
 }
 
-
 function showInvalid(node) {
-    node.parentElement.querySelector(".invalid-feedback").style.display = "block";
-    node.parentElement.querySelector(".valid-feedback").style.display = "none";
+  node.parentElement.querySelector(".invalid-feedback").style.display = "block";
+  node.parentElement.querySelector(".valid-feedback").style.display = "none";
 }
